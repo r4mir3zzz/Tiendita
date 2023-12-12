15bc07be83e8d4456d6a7c320e03d48434ca62ef
r4mir3zzz <lramireza616@gmail.com>
2023-12-12 13:40:54 -0600

    Semana #10

---
+++ src/main/java/com/tienda/dao/UsuarioDao.java
@@ -0,0 +1,19 @@
+package com.tienda.dao;
+
+import com.tienda.domain.Categoria;
+import com.tienda.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+/*Dao = Data acces objet*/
+public interface UsuarioDao 
+        extends JpaRepository<Usuario, Long> {
+    
+    public Usuario findByUsername(String username);
+
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
+}
